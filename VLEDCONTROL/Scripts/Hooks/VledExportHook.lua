9973bd4b212b89d7b5c9921d9e281845ad9e051f
Sp117UNSC <31255951+Sp117UNSC@users.noreply.github.com>
2021-10-25 02:08:18 +0300

    Added export hook
    onSimulationResume hook will let the player  update LED's even after changing aircraft without reloading mission

--- VLEDCONTROL/Scripts/Hooks/VledExportHook.lua
+++ VLEDCONTROL/Scripts/Hooks/VledExportHook.lua
@@ -6,6 +6,12 @@ function statistics.onSimulationStart()
     net.log('VLED.Export: loaded');
 end
 
+function statistics.onSimulationResume()
+    net.log("VLED.Export: loading");
+    net.dostring_in("export", "dofile(lfs.writedir()..[[Scripts\\Hooks\\vled\\VledExport.lua]])");
+    net.log('VLED.Export: loaded');
+end
+
 function statistics.onMissionLoadBegin()
     net.log('VLED.Export: Reloading user scripts...');
     DCS.reloadUserScripts();
